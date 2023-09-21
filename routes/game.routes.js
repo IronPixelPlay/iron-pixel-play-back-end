31d09aead1c74ed01c31b293e9c206b7155b88e5
Johanna Geßner <johanna-gessner@web.de>
2023-09-21 08:44:33 +0200

    retrieve a specific game by id from database

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -31,7 +31,7 @@ router.post("/games", isAuthenticated, (req, res, next) => {
         });
 });
 
-router.get("/games", (req, res, next) => {
+router.get("/games", isAuthenticated, (req, res, next) => {
     Game.find()
         .populate("review")
         .then(allGames => res.json(allGames))
@@ -42,5 +42,24 @@ router.get("/games", (req, res, next) => {
             });
         });
 })
+
+router.get("/games/:gameId", (req, res, next) => {
+    const { gameId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(gameId)) {
+        res.status(400).json({ message: "Specified id is not valid" });
+        return;
+    }
+
+    Game.findById(gameId)
+        .populate("review")
+        .then((game) => res.status(200).json(game))
+        .catch((err) => {
+            console.log("Error getting the user's game...", err);
+            res.status(500).json({
+                message: "We are sorry, we couldn't display your game",
+            });
+        });
+});
 
 module.exports = router;
