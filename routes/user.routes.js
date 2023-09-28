4c7e3ad1a1e6a2af04123fb381375e245e1a01c9
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-09-28 15:28:28 +0200

    Create routes editing profile & get user profile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt")
 const User = require("../models/User.model")
 const Review = require("../models/Review.model")
 const Game = require("../models/Game.model")
@@ -32,8 +32,60 @@ router.get("/user", isAuthenticated, async (req, res, next) => {
 });
 
 
+router.put('/user', isAuthenticated, (req, res, next) => {
+  const userId = req.payload._id;
 
-router.get("/user/")
+  const { email, password, name, image} = req.body
+  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!passwordRegex.test(password)) {
+    res.status(400).json({
+      message:
+      "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+    });
+    return;
+  }
+  const salt = bcrypt.genSaltSync(12);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+ 
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+ 
+  User.findByIdAndUpdate(userId, {email, password: hashedPassword, name, image}, { new: true })
+    .then((updatedProfile) => res.json(updatedProfile))
+    .catch((err) => {
+      console.log("Error updating the user's profile...", err);
+      res.status(500).json({
+        message: "We are sorry, we couldn't update your profile",
+      });
+    });
+});
+
+
+
+router.get("/user/:userId", isAuthenticated, async (req, res, next) => {
+  const userId = req.params.userId;
+
+  try {
+    const userData = await User.findById(userId);
+    const userGame = await Game.find({ user: userId });
+    const userReviews = await Review.find({ user: userId });
+
+    const profileInfo = {
+      user: userData,
+      game: userGame,
+      reviews: userReviews,
+    };
+
+    res.json(profileInfo);
+  } catch {
+    (error) => {
+      console.error(error);
+      res.status(500).json({ message: "Server error" });
+    };
+  }
+});
 
 
 module.exports = router;
